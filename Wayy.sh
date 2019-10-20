7b4f634283e871c122b993f0f8ab2d9706b1cfe6
DarkGans <56784467+DarkGans@users.noreply.github.com>
2019-10-20 14:48:22 +0700

    The Next
    Opp

---
+++ Wayy.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/bash
+#Tools SadapWhatsApp
+#Sadap WhatsApp
+#By MrTamfanX
+#Jangan DiRecord Tod!!!
+#Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+yellow='\033[33;1m'
+echo " "
+echo  $yellow "                      <|@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@|>             "
+echo  $cyan "                      <| [#] Nama Author Yang Asli Di Bawah [#]   |>              "
+echo  $cyan "                      <|                                    [#]   |>             "
+echo  $cyan "                      <| [#] Name: Dark4127                 [#]   |>             "
+echo  $cyan "                      <| [#] MyTeam: Medan Cyber Network22  [#]   |>             "
+echo  $cyan "                      <| [#] Contact: NO KONTAK             [#]   |>             "
+echo  $cyan "                      <| [#] Youtube : MCN OFFICIAL         [#]   |>             "
+echo  $yellow "                      <|@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@|>             "
+sleep 3
+echo "masukan password anda:"
+read password
+
+if [ $password == DarkGans ]; then
+  echo "Kamu COCOK sekale"
+fi
+sleep 2
+clear
+echo $purple"                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                   "
+echo $yellow "                       [#]       WHATSAPP TARGET       [#]                  "
+echo $purple"                        @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                   "
+echo $cyan"╭──[Masukan Code Verivikasi]>"
+   read -p"╰───────root@Dark4127=•>" Dark4127;
+if [ $Dark4127 = 2308 ] || [ $Dark4127 = 2308 ]
+then
+sleep 5
+clear
+echo$red"Loading..."
+sleep 5
+clear
+echo $cyan "                               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                "
+echo $purple "                                    [#] BERHASIL LOGIN [#]                         "
+echo $cyan "                               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                "
+echo $purple "                                         WAHYU ADITYA                                 "
+echo $cyan "                               @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                "
+echo $purple "                                  [#]  DUMP CHAT KORBAN [#]                        "
+sleep 3
+clear
+echo $cyan "                      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                     "
+echo $red  "                      @ [#] DAFTAR NAMA CHAT KORBAN [#]@                         "
+echo $cyan "                      @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                      "
+echo
+sleep 2
+clear
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                        "
+echo $cyan "                        01   Mutiara Sagala(Syng)                            "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                        "
+echo $cyan "                        02   Akbar Gumilang                               "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                        "
+echo $cyan "                        03   Kk Putri                                   "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                        "
+echo $cyan "                        04   SD NEGRI 064973                           "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                       "
+echo $cyan "                        05   Bg Arif                                    "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                       "
+echo $cyan "                        06   Mawar (Anak Tembung)                        "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                        "
+echo $cyan "                        07   XI-TKJ SMKN                                  "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                        "
+echo $cyan "                        08   Mawar                                        "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                         "
+echo $cyan "                        09  +62 857-2528-9509                              "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                         "
+echo $cyan "                        10   Kk Masha (Unyu)                               "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                         "
+echo $cyan "                        11   +62 822-7732-8614                             "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                         "
+echo $purple"                            210 CHAT DIARSIPKAN                          "
+echo $yellow"                         ☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠☠                        "
+echo $green "                       00.  exit                                         "
+fi
+if [ $King = 2 ] || [ $King = 02 ]
+then
